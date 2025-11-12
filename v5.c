8730d73f5ff0e76fdb72943f7a45157a988d91ab
Congojung <phungminhcong234@gmail.com>
2025-11-12 23:23:49 +0700

    Update v5.c

--- v5.c
+++ v5.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
-int main() {
-    int n;
-    printf("Nhap so phan tu cua mang: ");
-    scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++) {
-        printf("Nhap phan tu thu %d: ",i+1);
-        scanf("%d",&a[i]);
-    }
-    int maxcount=0,phantu=a[0];
-    for(int i=0;i<n;i++) {
-        int count=0;
-        for(int j=0;j<n;j++) {
-            if(a[i]==a[j]) count++;
-        }
-        if(count>maxcount) {
-            maxcount=count;
-            phantu=a[i];
-        }
-    }
-    printf("Phan tu xuat hien nhieu nhat la %d, xuat hien %d lan\n",phantu,maxcount);
-    return 0;
-}
 
+int timphantu_nhieunhat(int a[], int n) {
+  int maxcount = 0, phantu = a[0];
+  for (int i = 0; i < n; i++) {
+    int count = 0;
+    for (int j = 0; j < n; j++) {
+      if (a[i] == a[j]) count++;
+    }
+    if (count > maxcount) {
+      maxcount = count;
+      phantu = a[i];
+    }
+  }
+  return phantu;
+}
+int main() {
+  int n;
+  printf("nhap so phan tu: ");
+  scanf("%d", & n);
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    printf("nhap phan tu %d: ", i + 1);
+    scanf("%d", & a[i]);
+  }
+  int phantu = timphantu_nhieunhat(a, n);
+  int solan = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] == phantu) solan++;
+  }
+  printf("phan tu xuat hien nhieu nhat la %d, xuat hien %d lan\n", phantu, solan);
+  return 0;
+}
