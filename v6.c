6bf1c2a641dfed3ffa3ff684deaa9d4a35abfee6
Congojung <phungminhcong234@gmail.com>
2025-11-12 23:26:16 +0700

    Update v6.c

--- v6.c
+++ v6.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
+
 void bubblesort(int a[], int n) {
-    for (int i=0;i<n-1;i++) {
-        for (int j=0;j<n-i-1;j++) {
-            if (a[j]>a[j+1]) {
-                int temp =a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
-        }
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        int temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
     }
+  }
 }
 int main() {
-    int a[]={5,2,8,3,1,7};
-    int n=sizeof(a)/sizeof(a[0]);
-    printf("Mang ban dau: ");
-    for (int i=0;i<n;i++) {
-        printf("%d ", a[i]);
-    }
-    bubblesort(a,n);
-    printf("\nMang sau khi sap xep tang dan: ");
-    for (int i=0;i<n;i++) {
-        printf("%d ",a[i]);
-    }
-    return 0;
+  int n;
+  printf("nhap so phan tu: ");
+  scanf("%d", & n);
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    printf("nhap phan tu %d: ", i + 1);
+    scanf("%d", & a[i]);
+  }
+  printf("mang ban dau: ");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  bubblesort(a, n);
+  printf("\nmang sau khi sap xep tang dan: ");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  return 0;
 }
-
