bceb16439f60218764dfa4d051481d6db509a815
Congojung <phungminhcong234@gmail.com>
2025-11-12 23:46:00 +0700

    Update v7.c

--- v7.c
+++ v7.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-int timkiemso(int a[],int n,int x) {
-    int left=0,right=n-1;
-    while (left<=right) {
-        int mid=(left+right)/2;
-        if (a[mid]==x)
-            return mid;     
-        else if (a[mid]<x)
-            left=mid+1; 
-        else
-            right=mid-1; 
-    }
-    return -1; 
+
+int timkiemso(int a[], int n, int x) {
+  int left = 0, right = n - 1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (a[mid] == x)
+      return mid;
+    else if (a[mid] < x)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+  return -1;
 }
 int main() {
-    int a[]={1,3,5,7,9,11};
-    int n=6;
-    int x;
-    printf("Nhap so can tim: ");
-    scanf("%d",&x);
-    int kq=timkiemso(a,n,x);
-    if (kq!=-1)
-        printf("Tim thay %d tai vi tri %d\n",x,kq);
-    else
-        printf("Khong tim thay %d trong mang\n",x);
-    return 0;
+  int n;
+  printf("nhap so phan tu: ");
+  scanf("%d", & n);
+  int a[n];
+  printf("nhap cac phan tu (da sap xep tang dan):\n");
+  for (int i = 0; i < n; i++) {
+    printf("phan tu %d: ", i + 1);
+    scanf("%d", & a[i]);
+  }
+  int x;
+  printf("nhap so can tim: ");
+  scanf("%d", & x);
+  int kq = timkiemso(a, n, x);
+  if (kq != -1)
+    printf("tim thay %d tai vi tri %d\n", x, kq);
+  else
+    printf("khong tim thay %d trong mang\n", x);
+  return 0;
 }
-
+ 
